c0ff52e66032ea2843ade30ba982bb7eec5a4ae0
andrewmedhat123 <106705353+andrewmedhat2000@users.noreply.github.com>
2023-11-24 00:31:20 +0200

    Add files via upload

---
+++ src/components/Router/Router.jsx
@@ -0,0 +1,161 @@
+import React, { useState } from "react";
+import { Routes, Route } from "react-router";
+import Home from "../home/home";
+import Shop from "../shop/shop";
+import Cart from "../cart/cart";
+import Contact from "../contact/contact";
+import Login from "../login/login";
+import Homeproduct from "../home/home_product";
+import Nav from "../global/nav/nav";
+import Footer from "../global/footer/footer";
+import AddClient from "../shop/AddClient";
+import AddCardInfo from "../shop/AddCardInfo";
+import ReturnProduct from "../shop/ReturnProduct";
+import BuyProductQR from "../shop/BuyProductQR";
+const Rout = ({
+  shop,
+  Filter,
+  allcatefilter,
+  addtocart,
+  cart,
+  setCart,
+  setShop,
+}) => {
+  const [search, setSearch] = useState("");
+  const searchlength = (search || []).length === 0;
+  const searchproduct = () => {
+    if (searchlength) {
+      alert("Please Search Something !");
+      setShop(Homeproduct);
+    } else {
+      const searchfilter = Homeproduct.filter((x) => {
+        return x.cat === search;
+      });
+      setShop(searchfilter);
+    }
+  };
+  return (
+    <>
+      <Routes>
+        <Route path="/" element={<Login />} />
+        <Route
+          path="/home"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <Home addtocart={addtocart} /> <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/BuyProductQR/:id"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <BuyProductQR /> <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/cart"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <Cart cart={cart} setCart={setCart} />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/shop"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <Shop
+                shop={shop}
+                Filter={Filter}
+                allcatefilter={allcatefilter}
+                addtocart={addtocart}
+              />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/contact"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <Contact />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/addClient"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <AddClient />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/addCardInfo/:phone"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <AddCardInfo />
+              <Footer />
+            </>
+          }
+        />
+        <Route
+          path="/returnProduct"
+          element={
+            <>
+              <Nav
+                search={search}
+                setSearch={setSearch}
+                searchproduct={searchproduct}
+              />
+              <ReturnProduct />
+              <Footer />
+            </>
+          }
+        />
+      </Routes>
+    </>
+  );
+};
+
+export default Rout;
---
+++ src/components/login/login.jsx
@@ -0,0 +1,132 @@
+import React, { useState } from "react";
+import "../contact/contact.css";
+import { useNavigate } from "react-router-dom";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import axios from "axios";
+import { FormHelperText } from "@mui/material";
+import { AiFillEyeInvisible, AiFillEye } from "react-icons/ai";
+import { toast } from "react-toastify";
+import CircularProgress from "@mui/material/CircularProgress";
+
+const Login = () => {
+  const [passwordType, setPasswordType] = useState(false);
+  const [loading, setLoading] = useState(false);
+  const navigate = useNavigate();
+  const handelShowPassword = () => {
+    setPasswordType(!passwordType);
+  };
+  const check = () => {
+    const QR = localStorage.getItem("QrId");
+    console.log(QR);
+    if (QR) {
+      navigate(`/BuyProductQR/${QR}`);
+    } else {
+      navigate("/home");
+    }
+  };
+  const formik = useFormik({
+    initialValues: {
+      email: "",
+      password: "",
+    },
+    validationSchema: yup.object({
+      email: yup.string().email("Should be a valid email").required("Required"),
+      password: yup.string().required("Required").min(8),
+    }),
+    onSubmit: (values) => {
+      setLoading(true);
+      axios
+        .post("https://andrew-demo.onrender.com/user/loginforall", values)
+        .then((res) => {
+          setLoading(false);
+          toast.success(res.data.message);
+          localStorage.setItem("token", res.data.token);
+        })
+        .then(() => check())
+        .catch((e) => {
+          setLoading(false);
+
+          console.log(e);
+          toast.error(e.response.data.message);
+        });
+    },
+  });
+
+  return (
+    <>
+      <div className="contact">
+        <div style={{ marginTop: "300px" }}>
+          <div className="container">
+            <div className="form">
+              <h2>Login To System</h2>
+              <form onSubmit={formik.handleSubmit}>
+                <div className="box">
+                  <div className="lable">
+                    <h4>E-mail</h4>
+                  </div>
+                  <div className="input">
+                    <input
+                      type="text"
+                      name="email"
+                      onChange={formik.handleChange}
+                      value={formik.values.email}
+                      placeholder="E-MAIL"
+                      onBlur={formik.handleBlur}
+                    ></input>
+                    {formik.errors.email && formik.touched.email && (
+                      <FormHelperText sx={{ color: "red" }} id="email">
+                        {formik.errors.email}
+                      </FormHelperText>
+                    )}
+                  </div>
+                </div>
+                <div className="box">
+                  <div className="lable">
+                    <h4>Password</h4>
+                  </div>
+                  <div className="input">
+                    <div className="password-input">
+                      <input
+                        type={passwordType ? "text" : "password"}
+                        placeholder="PASSWORD"
+                        name="password"
+                        onChange={formik.handleChange}
+                        value={formik.values.password}
+                        onBlur={formik.handleBlur}
+                      ></input>
+                      <span
+                        className="showPassword"
+                        onClick={handelShowPassword}
+                      >
+                        {passwordType ? <AiFillEye /> : <AiFillEyeInvisible />}
+                      </span>
+                    </div>
+                    {formik.errors.password && formik.touched.password && (
+                      <FormHelperText sx={{ color: "red" }}>
+                        {formik.errors.password}
+                      </FormHelperText>
+                    )}
+                  </div>
+                </div>
+
+                <button
+                  type="sublit"
+                  style={loading ? { padding: "8px 38px" } : {}}
+                >
+                  {loading ? (
+                    <CircularProgress size="1rem" />
+                  ) : (
+                    <div>Login</div>
+                  )}
+                </button>
+              </form>
+            </div>
+          </div>
+        </div>
+      </div>
+    </>
+  );
+};
+
+export default Login;
